53d8584c5dc51d036742fd657ce071ac8ab6b40c
AJ <pinedalezephyr@gmail.com>
2018-11-21 15:42:35 -0700

    working with content provider

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -15,6 +15,10 @@
  */
 package com.example.android.pets.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -24,51 +28,202 @@ public final class PetContract {
 
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
-    private PetContract() {}
+    private PetContract() {
+    }
+
+    /**
+
+     * The "Content authority" is a name for the entire content provider, similar to the
+
+     * relationship between a domain name and its website. A convenient string to use for the
+
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+
+     * device.
+
+     */
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+
+
+    /**
+
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+
+     * the content provider.
+
+     */
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+
+
+    /**
+
+     * Possible path (appended to base content URI for possible URI's)
+
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+
+     */
+
+    public static final String PATH_PETS = "pets";
+
+
+    /**
+     * {@link ContentProvider} for Pets app.
+     */
+
+    public static class PetProvider extends ContentProvider {
+
+
+        /**
+         * Tag for the log messages
+         */
+
+        public static final String LOG_TAG = PetProvider.class.getSimpleName();
+        /**
+         * data base helper
+         */
+
+        private PetDbHelper mDbHelper;
+
+
+        /**
+         * Initialize the provider and the database helper object.
+         */
+
+        @Override
+
+        public boolean onCreate() {
+            mDbHelper = new PetDbHelper(getContext());
+
+
+            return true;
+
+        }
+
+
+        /**
+         * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
+         */
+
+        @Override
+
+        public Cursor query(Uri uri,String[] projection,String selection,String[] selectionArgs,
+
+                            String sortOrder) {
+
+            return null;
+
+        }
+
+
+        /**
+         * Insert new data into the provider with the given ContentValues.
+         */
+
+        @Override
+
+        public Uri insert(Uri uri,ContentValues contentValues) {
+
+            return null;
+
+        }
+
+
+        /**
+         * Updates the data at the given selection and selection arguments, with the new ContentValues.
+         */
+
+        @Override
+
+        public int update(Uri uri,ContentValues contentValues,String selection,String[] selectionArgs) {
+
+            return 0;
+
+        }
+
+
+        /**
+         * Delete the data at the given selection and selection arguments.
+         */
+
+        @Override
+
+        public int delete(Uri uri,String selection,String[] selectionArgs) {
+
+            return 0;
+
+        }
+
+
+        /**
+         * Returns the MIME type of data for the content URI.
+         */
+
+        @Override
+
+        public String getType(Uri uri) {
+
+            return null;
+
+        }
+
+    }
 
     /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+        /** The content URI to access the pet data in the provider */
 
-        /** Name of database table for pets */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+                /**
+         * Name of database table for pets
+         */
         public final static String TABLE_NAME = "pets";
 
         /**
          * Unique ID number for the pet (only for use in the database table).
-         *
+         * <p>
          * Type: INTEGER
          */
         public final static String _ID = BaseColumns._ID;
 
         /**
          * Name of the pet.
-         *
+         * <p>
          * Type: TEXT
          */
-        public final static String COLUMN_PET_NAME ="name";
+        public final static String COLUMN_PET_NAME = "name";
 
         /**
          * Breed of the pet.
-         *
+         * <p>
          * Type: TEXT
          */
         public final static String COLUMN_PET_BREED = "breed";
 
         /**
          * Gender of the pet.
-         *
+         * <p>
          * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
          * or {@link #GENDER_FEMALE}.
-         *
+         * <p>
          * Type: INTEGER
          */
         public final static String COLUMN_PET_GENDER = "gender";
 
         /**
          * Weight of the pet.
-         *
+         * <p>
          * Type: INTEGER
          */
         public final static String COLUMN_PET_WEIGHT = "weight";
